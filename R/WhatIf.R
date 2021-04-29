b1dcc3ab7f5f722056eb982d605862fe2f10d264
Andreas <andreas.hofheinz@outlook.com>
2021-04-29 18:05:27 +0200

    init for whatif

---
+++ R/WhatIf.R
@@ -0,0 +1,179 @@
+WhatIf = R6Class("WhatIf",
+  inherit = Counterfactuals,
+  
+  private = list(
+    X = NULL,
+    y_hat = NULL,
+    X_desired_outcome = NULL,
+    dist_vector = NULL,
+    n = NULL,
+    n_cores = NULL,
+    
+    preprocess = function() {
+      is_desired_outcome = (private$y_hat == private$desired_outcome)
+      private$X_desired_outcome = private$X[is_desired_outcome, ]
+    },
+    
+    calculate = function() {
+      private$dist_vector = private$compute_gower_dist(
+        private$x_interest, private$X_desired_outcome
+      )
+    },
+    
+    aggregate = function() {
+      X_temp = private$X_desired_outcome
+      X_temp$dist_x_interest = private$dist_vector
+      X_temp_ordered = X_temp[order(X_temp$dist_x_interest), ]
+      cfactuals = head(X_temp_ordered, private$n)
+      private$.results = private$make_results_list(cfactuals)
+    },
+    
+    compute_gower_dist = function(x_interest, X, n_cores = private$n_cores) {
+      gower_dist(x_interest, X, n_cores)
+    },
+    
+    one_hot_to_one_col = function(df) {
+      colnames(df)[apply(df, 1, which.max)]
+    },
+    
+    infer_X = function(X) {
+      if (is.null(X)) {
+        res = private$predictor$data$get.x()
+      } else {
+        res = X
+      }
+      res
+    }
+    
+  ),
+  
+  public = list(
+    
+    initialize = function(predictor, X = NULL, x_interest = NULL) {
+      
+      # TODO: Check if y is in X -> if yes remove and message
+      
+      private$predictor = predictor
+      private$X = as.data.table(private$infer_X(X))
+      y_hat_raw = predictor$predict(private$X)
+      private$check_that_classif_task(y_hat_raw)
+      y_hat_one_col = private$one_hot_to_one_col(y_hat_raw)
+      private$y_hat = y_hat_one_col
+    },
+    
+    find_counterfactuals = function(x_interest, desired_outcome, n = 1L, 
+                                    n_cores = parallel::detectCores() - 1) {
+      
+      # TODO: Check if desired_outcome is in private$y_hat
+      
+      private$x_interest = as.data.table(x_interest)
+      private$desired_outcome = desired_outcome
+      private$n = n
+      private$n_cores = n_cores
+      private$run()
+    }
+  )
+)
+
+library(doParallel)
+gower_dist <- function(x, data, n_cores) {
+
+  cl <- parallel::makeCluster(n_cores)
+  doParallel::registerDoParallel(cl)
+  on.exit(parallel::stopCluster(cl))
+  
+  # Deactivate multiprocessing in case of too less data
+  if (floor(nrow(data) / n_cores) == 0) {
+    n_cores = 1
+  }
+
+  # For numeric variables, build ranges (max-min) to be used in gower-distance.
+  datax <- rbind(data, x)
+  ranges = rep(NA, ncol(datax))
+  for (i in 1:ncol(datax)) {
+    col <- datax[[i]]
+    if (is.numeric(col)) {
+      ranges[i] <- max(col, na.rm = TRUE) - min(col, na.rm = TRUE)
+    }
+  }
+
+  chunk_size = floor(nrow(data) / n_cores)
+  dist_vector = foreach(
+      i = 1:n_cores, .packages = c("StatMatch"), .combine = "c", .export = "gower_dist"
+    ) %dopar% {
+      from = (i - 1) * chunk_size + 1
+      to = i * chunk_size
+      if (i == n_cores) {
+        to = nrow(data)
+      }
+      dist_vector = as.vector(gower.dist(x, data[from:to,], rngs = ranges))
+    }
+  
+  dist_vector
+}
+
+
+# set.seed(123456)
+# iris_sub <- iris[sample(nrow(iris), 20), ]
+# oneinst = iris_sub[7, ]
+# target = as.vector(oneinst[5]) # virginica
+# x.interest = as.vector(oneinst[-5])
+# desired_outcome = "setosa"
+# iris_sub_desired_outcome = subset(iris_sub, Species == desired_outcome)
+# wi <- WhatIf$new(data = iris_sub)
+# wi$find_counterfactuals(x.interest, "setosa", n = 3L)
+# wi$results
+
+# testi = gower_dist(x.interest, iris_sub_desired_outcome[, -5],  n_cores = 7)
+# testi
+# iris_sub_desired_outcome[which.min(testi), ]
+
+
+# library("randomForest")
+# rf <- randomForest(Species ~ ., data = iris, ntree = 20)
+# mod <- Predictor$new(rf, data = iris)
+# preds = mod$predict(iris[50:55, ])
+# oneinst = iris[7, ]
+# x_interest = as.vector(oneinst[-5])
+# 
+# wi <- WhatIf$new(mod, X = iris[, -5])
+# wi$find_counterfactuals(x_interest, "setosa", n = 3L)
+# wi$results
+# 
+# library(mlbench)
+# data(PimaIndiansDiabetes)
+# dim(PimaIndiansDiabetes)
+# levels(PimaIndiansDiabetes$diabetes)
+# head(PimaIndiansDiabetes)
+# 
+# 
+# 
+# 
+# rf <- randomForest(diabetes ~ ., data = PimaIndiansDiabetes, ntree = 20)
+# mod <- Predictor$new(rf)
+# oneinst = PimaIndiansDiabetes[7, -9]
+# wi <- WhatIf$new(mod, X = PimaIndiansDiabetes[, -9])
+# wi$find_counterfactuals(oneinst, "neg", n = 3L)
+# wi$results
+# 
+# 
+# gower_dist(unlist(oneinst), PimaIndiansDiabetes[, -9],  n_cores = 3)
+
+# library("randomForest")
+# testi = mtcars
+# testi$am = as.factor(testi$am)
+# testi$vs = as.factor(testi$vs)
+# testi$cyl = as.factor(testi$cyl)
+# rf <- randomForest(am ~ ., data = testi[, c("am", "vs", "cyl")], ntree = 20)
+# mod_class = Predictor$new(rf, testi[, c("am", "vs", "cyl")])
+# 
+# wi = WhatIf$new(mod_class)
+# oneinst = head(subset(testi, select = c(vs, cyl)), 1L)
+# n = 3L
+# wi$find_counterfactuals(oneinst, "0", n = n, n_cores = 1L)
+# wi$results
+# 
+# wi$results$counterfactuals_diff[1, 1:7]
+
+
+
---
+++ tests/testthat/test-WhatIf.R
@@ -0,0 +1,102 @@
+library(checkmate)
+library(data.table)
+library(randomForest)
+
+set.seed(54542142)
+
+iris_sub = iris[c(1:5, 50:55, 100:105), ]
+rf = randomForest(Species ~ ., data = iris_sub, ntree = 2L)
+mod_class = Predictor$new(rf, iris_sub)
+
+test_that("Returns correct output format for numeric columns only", {
+  wi = WhatIf$new(mod_class)
+  oneinst = head(subset(iris_sub, select = -Species), 1L)
+  n = 3L
+  wi$find_counterfactuals(oneinst, "virginica", n = n, n_cores = 1L)
+  res = wi$results
+  expect_list(res, len = 2L)
+  cfs = res$counterfactuals
+  cfs_diff = res$counterfactuals_diff
+  expect_data_table(cfs, nrows = n)
+  expected_cols = c(colnames(iris_sub)[-5], "dist_x_interest", "nr_changed", "pred")
+  expect_true(all(colnames(cfs) == expected_cols))
+  expect_data_table(cfs_diff, nrows = n)
+  expect_true(all(colnames(cfs_diff) == expected_cols))
+  expected_diff = as.data.table(sweep(wi$results$counterfactuals[, 1:4], 2, as.numeric(oneinst)))
+  expect_equal(expected_diff, wi$results$counterfactuals_diff[, 1:4])
+})
+
+
+test_that("Returns correct output format for factor columns only", {
+  df = mtcars[, c("am", "vs", "cyl")]
+  df$am = as.factor(df$am)
+  df$vs = as.factor(df$vs)
+  df$cyl = as.factor(df$cyl)
+  rf <- randomForest(am ~ ., data = df, ntree = 5L)
+  mod = Predictor$new(rf, df)
+  wi = WhatIf$new(mod)
+  oneinst = df[1L, c("vs", "cyl")]
+  n = 5L
+  wi$find_counterfactuals(oneinst, "0", n = n, n_cores = 1L)
+  res = wi$results
+  expect_list(res, len = 2L)
+  cfs = res$counterfactuals
+  cfs_diff = res$counterfactuals_diff
+  expect_data_table(cfs, nrows = n)
+  expected_cols = c(colnames(df)[-1L], "dist_x_interest", "nr_changed", "pred")
+  expect_true(all(colnames(cfs) == expected_cols))
+  expect_data_table(cfs_diff, nrows = n)
+  expect_true(all(colnames(cfs_diff) == expected_cols))
+})
+
+test_that("Returns correct output format for factor and numeric columns", {
+  df = mtcars
+  df$am = as.factor(df$am)
+  df$vs = as.factor(df$vs)
+  rf <- randomForest(am ~ ., data = df, ntree = 5L)
+  mod = Predictor$new(rf, df)
+  wi = WhatIf$new(mod)
+  oneinst = head(subset(df, select = -am), n = 1L)
+  n = 5L
+  wi$find_counterfactuals(oneinst, "0", n = n, n_cores = 1L)
+  res = wi$results
+  expect_list(res, len = 2L)
+  cfs = res$counterfactuals
+  cfs_diff = res$counterfactuals_diff
+  expect_data_table(cfs, nrows = n)
+  expected_cols = c(colnames(oneinst), "dist_x_interest", "nr_changed", "pred")
+  expect_true(all(colnames(cfs) == expected_cols))
+  expect_data_table(cfs_diff, nrows = n)
+  expect_true(all(colnames(cfs_diff) == expected_cols))
+})
+
+
+test_that("Parallelization leads to same results as sequential execution", {
+  oneinst = head(subset(iris_sub, select = -Species), 1)
+  wi = WhatIf$new(mod_class)
+  wi$find_counterfactuals(oneinst, "virginica", n = 10L, n_cores = parallel::detectCores() - 1L)
+  res_par = wi$results
+  wi$find_counterfactuals(oneinst, "virginica", n = 10L, n_cores = 1L)
+  res_seq = wi$results
+  expect_identical(res_par, res_seq)
+})
+
+test_that("Init works for classification tasks only", {
+  
+  # Regression task
+  rf_regr = randomForest::randomForest(mpg ~ ., data = mtcars, ntree = 2L)
+  mod_regr = Predictor$new(rf_regr)
+  expect_error(WhatIf$new(mod_regr), "only works for classification")
+
+  # Classification task
+  expect_error(WhatIf$new(mod_class), NA)
+
+  # The type of the task is inferred using the `inferTaskFromPrediction` from the iml package.
+  # Check that possible changes to this function don't break the code.
+  pred_regr = mod_regr$predict(mtcars[1:2, ])
+  expect_equal(iml:::inferTaskFromPrediction(pred_regr), "regression")
+  pred_class = mod_class$predict(iris_sub[1:2, ])
+  expect_equal(iml:::inferTaskFromPrediction(pred_class), "classification")
+})
+
+
