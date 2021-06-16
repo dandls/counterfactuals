3748eaa1aee822d69ec902fc31db199e3c40ff57
Andreas <andreas.hofheinz@outlook.com>
2021-06-16 18:14:16 +0200

    refactoring

--- R/utils_moc.R
+++ R/utils_moc.R
@@ -58,11 +58,11 @@ MutatorReset = R6::R6Class("MutatorReset",
     initialize = function(x_interest, p_mut_use_orig, max_changed) {
       private$.x_interest = assert_data_table(x_interest)
       assert_numeric(p_mut_use_orig, lower = 0, upper = 1, len = 1L, any.missing = FALSE)
-      assert_integerish(max_changed, lower = 0, len = 1L, any.missing = FALSE)
+      assert_integerish(max_changed, lower = 0, len = 1L, any.missing = FALSE, null.ok = TRUE)
 
       params = ps(
-        max_changed = p_int(max_changed),
-        p_mut_use_orig = p_dbl(p_mut_use_orig)
+        max_changed = p_int(special_vals = list(NULL)),
+        p_mut_use_orig = p_dbl()
       )
       params$values = list(
         "max_changed" = max_changed,
@@ -104,7 +104,7 @@ MutatorReset = R6::R6Class("MutatorReset",
 
         x_interest_sub = private$.x_interest[, names(values_mutated), with = FALSE]
 
-        n_changes = count_changes(values_mutated[i, ], x_interest_sub)
+        n_changes = counterfactuals:::count_changes(values_mutated[i, ], x_interest_sub)
 
         if (!is.null(max_changed)) {
           if (n_changes > max_changed) {
@@ -116,7 +116,6 @@ MutatorReset = R6::R6Class("MutatorReset",
         }
 
       }
-      # print(counterfactuals:::count_changes(values_mutated, private$.x_interest))
       values_mutated
     }
   )
@@ -131,10 +130,10 @@ RecombinatorReset = R6::R6Class("RecombinatorReset",
     initialize = function(x_interest, p_mut_use_orig, max_changed) {
       private$.x_interest = assert_data_table(x_interest)
       assert_numeric(p_mut_use_orig, lower = 0, upper = 1, len = 1L, any.missing = FALSE)
-      assert_integerish(max_changed, lower = 0, len = 1L, any.missing = FALSE)
+      assert_integerish(max_changed, lower = 0, len = 1L, any.missing = FALSE, null.ok = TRUE)
       params = ps(
-        max_changed = p_int(max_changed),
-        p_mut_use_orig = p_dbl(p_mut_use_orig)
+        max_changed = p_int(special_vals = list(NULL)),
+        p_mut_use_orig = p_dbl()
       )
       params$values = list(
         "max_changed" = max_changed,
@@ -183,3 +182,75 @@ RecombinatorReset = R6::R6Class("RecombinatorReset",
     }
   )
 )
+
+make_moc_mutator = function(ps, max_changed, sdevs_num_feats, p_mut_gen, p_mut_use_orig) {
+  ops_m_list = list()
+  if ("ParamDbl" %in% ps$class) {
+    ops_m_list[["ParamDbl"]] = miesmuschel::mut(
+      "maybe", miesmuschel::mut("gauss", sdev = sdevs_num_feats), miesmuschel::mut("null"),
+      p = p_mut_gen
+    )
+  }
+  if ("ParamInt" %in% ps$class) {
+    ops_m_list[["ParamInt"]] = miesmuschel::mut(
+      "maybe", miesmuschel::mut("gauss"), miesmuschel::mut("null"),
+      p = p_mut_gen
+    )
+  }
+  if ("ParamFct" %in% ps$class) {
+    n_facts = sum("ParamFct" == ps$class)
+    ls_op_factor = rep(list(miesmuschel::mut("unif", can_mutate_to_same = FALSE)), n_facts)
+    names(ls_op_factor) = ps$ids()[which(ps$class == "ParamFct")]
+    ops_m_list = c(ops_m_list, ls_op_factor)
+  }
+
+  op_m_seq1 = miesmuschel::mut("combine", operators = ops_m_list)
+  op_m_seq2 = MutatorReset$new(x_interest, p_mut_use_orig, max_changed)
+  miesmuschel::mut("sequential", list(op_m_seq1, op_m_seq2))
+}
+
+
+make_moc_recombinator = function(ps, max_changed, sdevs_num_feats, p_rec_use_orig) {
+  # TODO: DO we need probs here?
+  ops_r_list = list()
+  if ("ParamDbl" %in% ps$class) {
+    # TODO: Replace this with "simulated binary crossover recombinator"
+    ops_r_list[["ParamDbl"]] = miesmuschel::rec("xounif")
+  }
+  if ("ParamInt" %in% ps$class) {
+    ops_r_list[["ParamInt"]] = miesmuschel::rec("xounif")
+  }
+  if ("ParamFct" %in% ps$class) {
+    n_facts = sum("ParamFct" == ps$class)
+    ls_op_factor = rep(list(miesmuschel::rec("xounif")), n_facts)
+    names(ls_op_factor) = names(ps$class)[which(ps$class == "ParamFct")]
+    ops_r_list = c(ops_r_list, ls_op_factor)
+  }
+  op_r_seq_1 = miesmuschel::rec("combine", operators = ops_r_list)
+  op_r_seq_2 = RecombinatorReset$new(x_interest, p_rec_use_orig, max_changed = 2L)
+  miesmuschel::rec("sequential", list(op_r_seq_1, op_r_seq_2))
+}
+
+
+make_moc_pop_initializer = function(ps, x_interest, max_changed) {
+  if (is.null(max_changed)) {
+    pop_initializer = paradox::generate_design_random
+  } else {
+    pop_initializer = function(param_set, n) {
+      my_design = paradox::SamplerUnif$new(ps)$sample(n)
+      x_interest_reorderd = x_interest[, names(my_design$data), with = FALSE]
+      n_changes = counterfactuals:::count_changes(my_design$data, x_interest_reorderd)
+
+      for (i in 1:nrow(my_design$data)) {
+        if (n_changes[i] > max_changed) {
+          pos_diff = which(my_design$data[i, ] != x_interest_reorderd)
+          to_be_reverted = sample(pos_diff, size = n_changes[i] - max_changed)
+          to_be_reverted = names(my_design$data)[to_be_reverted]
+          my_design$data[i, (to_be_reverted) := x_interest_reorderd[, to_be_reverted, with = FALSE]]
+        }
+      }
+      my_design
+    }
+  }
+  pop_initializer
+}
