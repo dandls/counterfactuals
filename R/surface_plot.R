15805aa004df3e929a61b253a3094268a8897128
Andreas <andreas.hofheinz@outlook.com>
2021-06-17 14:56:11 +0200

    improvements surface plot
    - plot x_interest after counterfactuals
    - fix mixed plot

--- R/surface_plot.R
+++ R/surface_plot.R
@@ -5,22 +5,23 @@ make_surface_plot = function(grid_size, param_set, cfactuals_plotted, x_interest
   dt_grid = make_ice_curve_area(predictor, x_interest, grid_size, param_set_sub, pred_column)
   x_feat_name = feature_names[1L]
   y_feat_name = feature_names[2L]
-  
+
   if (param_set_sub$all_numeric) {
     # TODO: adapt this for hard classification
     p = ggplot2::ggplot(data = dt_grid, ggplot2::aes_string(x = x_feat_name, y = y_feat_name)) + 
       ggplot2::geom_raster(ggplot2::aes_string(fill = "pred")) +
       ggplot2::geom_contour(ggplot2::aes_string(z = "pred"), colour = "white") +
-      ggplot2::geom_point(ggplot2::aes_string(x = x_feat_name, y = y_feat_name), x_interest, colour = "white") +
       ggplot2::guides(z = ggplot2::guide_legend(title = "pred")) +
       ggplot2::theme_bw() +
       ggplot2::theme(legend.position = "left")
-
+    
     if (nrow(cfactuals_plotted) > 0L) {
       p = p + 
         ggplot2::geom_point(ggplot2::aes_string(x = x_feat_name, y = y_feat_name), cfactuals_plotted, colour = "black") +
         ggplot2::geom_rug(ggplot2::aes_string(x = x_feat_name, y = y_feat_name), cfactuals_plotted)
     }
+    
+    p = p + ggplot2::geom_point(ggplot2::aes_string(x = x_feat_name, y = y_feat_name), x_interest, colour = "white")
     
   } else if (param_set_sub$all_categorical) {
     p = ggplot2::ggplot(dt_grid, ggplot2::aes_string(x_feat_name, y_feat_name)) +
@@ -50,7 +51,7 @@ make_surface_plot = function(grid_size, param_set, cfactuals_plotted, x_interest
     if (nrow(cfactuals_plotted) > 0L) {
       p = p +
         ggplot2::geom_point(ggplot2::aes_string(x = num_feature, y = "pred"), cfactuals_plotted, colour = "black") +
-        ggplot2::geom_rug(ggplot2::aes_string(x = x_feat_name, y = "pred"), cfactuals_plotted)
+        ggplot2::geom_rug(ggplot2::aes_string(x = num_feature, y = "pred"), cfactuals_plotted)
     }
     
     p = p +
