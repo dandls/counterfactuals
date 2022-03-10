23001146f6a9652bc89fa132fbc9a6049cfd5966
susanne-207 <dandl.susanne@gmail.com>
2022-03-10 11:25:31 +0100

    Update parameters MOCClassif & -Regr according to irace

--- R/MOCRegr.R
+++ R/MOCRegr.R
@@ -37,15 +37,15 @@ MOCRegr = R6::R6Class("MOCRegr", inherit = CounterfactualMethodRegr,
     #' @param n_generations (`integerish(1)`)\cr  
     #'   The number of generations. Default is `175L`.   
     #' @param p_rec (`numeric(1)`)\cr  
-    #'   Probability with which an individual is selected for recombination. Default is `0.57`.
+    #'   Probability with which an individual is selected for recombination. Default is `0.66`.
     #' @param p_rec_gen (`numeric(1)`)\cr  
-    #'   Probability with which a feature/gene is selected for recombination. Default is `0.85`.  
+    #'   Probability with which a feature/gene is selected for recombination. Default is `0.73`.  
     #' @param p_mut (`numeric(1)`)\cr  
-    #'   Probability with which an individual is selected for mutation. Default is `0.79`.    
+    #'   Probability with which an individual is selected for mutation. Default is `0.8`.    
     #' @param p_mut_gen (`numeric(1)`)\cr  
-    #'   Probability with which a feature/gene is selected for mutation. Default is `0.56`.   
+    #'   Probability with which a feature/gene is selected for mutation. Default is `0.71`.   
     #' @param p_mut_use_orig (`numeric(1)`)\cr  
-    #'   Probability with which a feature/gene is reset to its original value in `x_interest` after mutation. Default is `0.32`.    
+    #'   Probability with which a feature/gene is reset to its original value in `x_interest` after mutation. Default is `0.26`.    
     #' @param k (`integerish(1)`)\cr  
     #'   The number of data points to use for the forth objective. Default is `1L`.
     #' @param weights (`numeric(1) | numeric(k)` | `NULL`)\cr  
@@ -66,8 +66,8 @@ MOCRegr = R6::R6Class("MOCRegr", inherit = CounterfactualMethodRegr,
     #'  `x`, `y`, and `data` and return a `numeric` matrix with `nrow(x)` rows and `nrow(y)` columns. 
     #'  If set to `NULL` (default), then Gower distance (Gower 1971) is used.
     initialize = function(predictor, epsilon = NULL, fixed_features = NULL, max_changed = NULL, mu = 20L,
-                          n_generations = 175L, p_rec = 0.57, p_rec_gen = 0.85, p_mut = 0.79,
-                          p_mut_gen = 0.56, p_mut_use_orig = 0.32, k = 1L, weights = NULL, lower = NULL, upper = NULL, 
+                          n_generations = 175L, p_rec = 0.66, p_rec_gen = 0.73, p_mut = 0.80, 
+                          p_mut_gen = 0.71, p_mut_use_orig = 0.26, k = 1L, weights = NULL, lower = NULL, upper = NULL, 
                           init_strategy = "random", use_conditional_mutator = FALSE, quiet = FALSE, distance_function = NULL) {
       
       if (is.null(distance_function)) {
@@ -104,11 +104,13 @@ MOCRegr = R6::R6Class("MOCRegr", inherit = CounterfactualMethodRegr,
           stop("Package 'basefun' needed for this function to work. Please install it.", call. = FALSE)
         }
         
+        nams_cs = names(predictor$data$X)
+        nams_cs = setdiff(nams_cs, fixed_features)
         private$conditional_sampler = sapply(
-          names(predictor$data$X), function(fname) ConditionalSampler$new(predictor$data$X, fname),
+          nams_cs, function(fname) ConditionalSampler$new(predictor$data$X, fname),
           simplify = FALSE, USE.NAMES = TRUE
         )
-        names(private$conditional_sampler) = names(predictor$data$X)
+        names(private$conditional_sampler) = nams_cs
       }
       
       private$epsilon = epsilon
